53bb2ba4a02ac7daa03940d61cd81a597e6c665b
Wyneg <wynegsrhuntar@gmail.com>
2020-11-26 12:03:55 -0300

    Update index.js
    Cambiando, otra vez listen

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ const app = express();
 
 const path = require('path');
 
-//app.set('port', 3000);
-
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Server en puerto : ', process.env.PORT);
-})
+app.listen(process.env.PORT || 5000)
