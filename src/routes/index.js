fb46c2204b49b8a5edb7016b2043d5357c98a331
Wyneg <wynegsrhuntar@gmail.com>
2020-11-26 13:55:09 -0300

    Update index.js
    !!

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile('app/src/views/index.html');
 });
 
 module.exports = router;
