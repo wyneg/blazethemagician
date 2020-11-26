9f28f177a62b4387ff686b1633e9079b4e501088
Wyneg <wynegsrhuntar@gmail.com>
2020-11-26 12:59:15 -0300

    Update index.js
    No se porque no me pesca

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile('/blazethemagician/src/views/index.html');
+    res.sendFile('/wyneg/blazethemagician/src/views/index.html');
 });
 
 module.exports = router;
