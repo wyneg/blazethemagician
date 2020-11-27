4bb11972a015deeef39979e8f3b215cafe16b59c
wyneg <wynegsrhuntar@gmail.com>
2020-11-27 14:00:22 -0300

    Shit

--- src/public/js/hechizos.js
+++ src/public/js/hechizos.js
@@ -3,12 +3,41 @@ $(function () {
 	"use strict";
 
 	function prenderAntorchas() {
-		prenderAntorchaIzq();
-		prenderAntorchaDer();
 		capLeidos();
+		animAntorchas();
 	}
 
-	function prenderAntorchaDer() {
+	function animAntorchas () {
+		setTimeout(function () {
+		  $("#izqOff").attr("src", "images/Torch-izqOn.gif");
+		  $("#negrito1").css("opacity", "0.5");
+		  $(".torch").css("background", "linear-gradient(to right, #F2F5A9, #FF8000, black)");
+		
+		  setTimeout(function () {
+			$("#derOff").attr("src", "images/Torch-derOn.gif");
+			$("#negrito1").css("opacity", "0.5");
+			$(".torch").css("background", "linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000)");
+			
+				setTimeout(function () {
+					$("#negrito1").css("opacity", "1");
+					$(".torch").css("background", "transparent");
+					$(".img").css("background", "");
+					$(".caca").css("pointer-events", "all");
+				}, 400);
+				
+			         setTimeout(function () {
+				        $("#negrito1").css("opacity", "1");
+				        $(".torch").css("background", "transparent");
+				        $(".img").css("background", "");
+				        $(".caca").css("pointer-events", "all");
+			         }, 400);
+			
+		  }, 1000);
+		
+	}, 1000);
+	};
+
+	$("#derOff").on("click", function () {
 		$("#derOff").attr("src", "images/Torch-derOn.gif");
 		if ($("#izqOff").attr('src') === 'images/Torch-izqOn.gif') {
 			$("#negrito1").css("opacity", "1");
@@ -19,9 +48,9 @@ $(function () {
 			$("#negrito1").css("opacity", "0.5");
 			$(".torch").css("background", "linear-gradient(to left, transparent, #FF8000, black)");
 		}
-	}
+	});
 	
-	function prenderAntorchaIzq() {
+	$("#izqOff").on("click", function () {
 		$("#izqOff").attr("src", "images/Torch-izqOn.gif");
 		if ($("#derOff").attr('src') === 'images/Torch-derOn.gif') {
 			$("#negrito1").css("opacity", "1");
@@ -32,14 +61,6 @@ $(function () {
 			$("#negrito1").css("opacity", "0.5");
 			$(".torch").css("background", "linear-gradient(to right, transparent, #FF8000, black)");
 		}
-	}
-
-	$("#izqOff").on("click", function () {
-		prenderAntorchaIzq();
-	});
-	
-	$("#derOff").on("click", function () {
-		prenderAntorchaDer();
 	});
 	
 	$("#izqOff").on("dblclick", function () {
--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -4,52 +4,14 @@ $(function () {
     $("a.dirweb").click(function (e) {
         e.preventDefault();
      });
-    
-    function antorchas() {
-        $("#izqOff").attr("src", "images/Torch-izqOff.gif");
-        $("#derOff").attr("src", "images/Torch-derOff.gif");
-        $("#negrito1,#negrito2").css("opacity", "0");
-    }
 
-    function animAntorchas () {
-		setTimeout(function () {
-		  $("#izqOff").attr("src", "images/Torch-izqOn.gif");
-		  $("#negrito1").css("opacity", "0.5");
-		  $(".torch").css("background", "linear-gradient(to right, #F2F5A9, #FF8000, black)");
-		
-		  setTimeout(function () {
-			$("#derOff").attr("src", "images/Torch-derOn.gif");
-			$("#negrito1").css("opacity", "0.5");
-			$(".torch").css("background", "linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000)");
-			
-				setTimeout(function () {
-					$("#negrito1").css("opacity", "1");
-					$(".torch").css("background", "transparent");
-					$(".img").css("background", "");
-					$(".caca").css("pointer-events", "all");
-				}, 400);
-				
-			         setTimeout(function () {
-				        $("#negrito1").css("opacity", "1");
-				        $(".torch").css("background", "transparent");
-				        $(".img").css("background", "");
-				        $(".caca").css("pointer-events", "all");
-			         }, 400);
-			
-		  }, 1000);
-		
-	}, 1000);
-	};
-
-    $("#inicio1").on("click", function () {
+     $("#inicio1").on("click", function () {
         fuegos(1);
         antorchas();
 	});
 	
-	
 	$("#capitulos1").on("click", function () {
         fuegos(2);
-        animAntorchas();
 	});
     
     $("#galeria1").on("click", function () {
@@ -62,6 +24,12 @@ $(function () {
         antorchas();
 	});
     
+    function antorchas() {
+        $("#izqOff").attr("src", "images/Torch-izqOff.gif");
+        $("#derOff").attr("src", "images/Torch-derOff.gif");
+        $("#negrito1,#negrito2").css("opacity", "0");
+    }
+
     function fuegos(llama) {
         
         if (llama === 1) {
