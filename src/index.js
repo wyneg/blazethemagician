f0a9b0440cb5f1b91b197bea86a90c04b40b8c96
wyneg <wynegsrhuntar@gmail.com>
2020-11-27 09:29:36 -0300

    port... anana!

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(app.get('port'), () => {
-    console.log('Server en puerto : ', app.get('port'));
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server en puerto : ', process.env.PORT);
 });
 
 //Cliente base de datos
