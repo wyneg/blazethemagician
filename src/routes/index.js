0806ff8cbae26dbc24d7ea280ebafbdce5b67e9e
wyneg <wynegsrhuntar@gmail.com>
2020-11-27 09:16:48 -0300

    path

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+console.log(__dirname);
+
 router.get('/', (req, res) => {
     res.sendFile('/app/src/views/index.html');
 });
