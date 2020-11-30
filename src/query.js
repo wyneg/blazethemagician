aef41e51000c874c61cee80ccae4c177914e9302
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 15:47:11 -0300

    reverting  some changes

--- src/query.js
+++ src/query.js
@@ -26,12 +26,6 @@ const client = new Client(connection);
 
 client.connect();
 
-/*router.get('/capitulos/:id', (req, res, next) => {
-    res.render('/app/src/views/galeria.html', {output : req.params.id});
-    console.log(req.params.id);
-});*/
-
-/*+ String(val)*/
 client.query('SELECT numcaps FROM blazecaps WHERE numcaps = 1')
 .then(response => {
     console.log(response.rows);
