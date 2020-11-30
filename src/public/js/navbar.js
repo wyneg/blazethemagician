f0096a9768fdd3acaa6bd0680bd956818f7e6ce7
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 11:50:35 -0300

    workaround (?) for llamas

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -2,22 +2,30 @@ $(function () {
     "use strict";
 
     $("#inicio1").on("click", function () {
-        $("#fuego1").attr("src", "images/favicon11.gif");
-        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        window.onload = function () {
+            $("#fuego1").attr("src", "images/favicon11.gif");
+            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        }
     });
     
     $("#capitulos1").on("click", function () {
-        $("#fuego2").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        window.onload = function () {
+            $("#fuego2").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        }
     });
     
     $("#galeria1").on("click", function () {
-        $("#fuego3").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+        window.onload = function () {
+            $("#fuego3").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+        }
     });
     
     $("#autor1").on("click", function () {
-        $("#fuego4").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
+        window.onload = function () {
+            $("#fuego4").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
+        } 
     });
 });
