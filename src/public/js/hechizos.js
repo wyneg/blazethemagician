f4624258a0b01ca4dbed6f06f4b4e009a3ed5ac6
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 10:22:34 -0300

    jquery to html dom

--- src/public/js/hechizos.js
+++ src/public/js/hechizos.js
@@ -5,27 +5,41 @@ $(function () {
 	//$('#capitulos1').on('click', function() {
 	function hagaseLaLuz() {
 		setTimeout(function () {
-		  $("#izqOff").attr("src", "images/Torch-izqOn.gif");
-		  $("#negrito1").css("opacity", "0.5");
-		  $(".torch").css("background", "linear-gradient(to right, #F2F5A9, #FF8000, black)");
+		  //$("#izqOff").attr("src", "images/Torch-izqOn.gif");
+		  document.getElementById("izqOff").setAttribute("src", "images/Torch-izqOn.gif");
+		  //$("#negrito1").css("opacity", "0.5");
+		  document.getElementById("negrito1").setAttribute("style", "opacity: 0.5");
+		  //$(".torch").css("background", "linear-gradient(to right, #F2F5A9, #FF8000, black)");
+		  document.getElementsByClassName("torch").setAttribute("style", "background: linear-gradient(to right, #F2F5A9, #FF8000, black");
 		
 		  setTimeout(function () {
-			$("#derOff").attr("src", "images/Torch-derOn.gif");
-			$("#negrito1").css("opacity", "0.5");
-			$(".torch").css("background", "linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000)");
+			//$("#derOff").attr("src", "images/Torch-derOn.gif");
+			document.getElementById("derOff").setAttribute("src", "images/Torch-derOn.gif");
+			//$("#negrito1").css("opacity", "0.5");
+			document.getElementById("negrito1").setAttribute("style", "opacity: 0.5");
+			//$(".torch").css("background", "linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000)");
+			document.getElementsByClassName("torch").setAttribute("style", "background: linear-gradient(to left, #F2F5A9, #FAAC58, #FF8000");
 			
 				setTimeout(function () {
-					$("#negrito1").css("opacity", "1");
-					$(".torch").css("background", "transparent");
-					$(".img").css("background", "");
-					$(".caca").css("pointer-events", "all");
+					//$("#negrito1").css("opacity", "1");
+					document.getElementById("negrito1").setAttribute("style", "opacity: 1");
+					//$(".torch").css("background", "transparent");
+					document.getElementsByClassName("torch").setAttribute("style", "background: transparent");
+					//$(".img").css("background", "");
+					document.getElementsByClassName("img").setAttribute("style", "background: ''");
+					//$(".caca").css("pointer-events", "all");
+					document.getElementsByClassName("caca").setAttribute("style", "pointer-events: all");
 				}, 400);
 				
 			         setTimeout(function () {
-				        $("#negrito1").css("opacity", "1");
-				        $(".torch").css("background", "transparent");
-				        $(".img").css("background", "");
-				        $(".caca").css("pointer-events", "all");
+						//$("#negrito1").css("opacity", "1");
+						document.getElementById("negrito1").setAttribute("style", "opacity: 1");
+						//$(".torch").css("background", "transparent");
+						document.getElementsByClassName("torch").setAttribute("style", "background: transparent");
+						//$(".img").css("background", "");
+						document.getElementsByClassName("img").setAttribute("style", "background: ''");
+						//$(".caca").css("pointer-events", "all");
+						document.getElementsByClassName("caca").setAttribute("style", "pointer-events: all");
 			         }, 400);
 			
 		  }, 1000);
--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,11 +1,6 @@
 $(function () {
     "use strict";
     
-    /*$("a.dirweb").click(function (e) {
-        e.preventDefault();
-     });*/
-
-    
     $('#inicio1').on('load', function() {
         $("#fuego1").attr("src", "images/favicon11.gif");
         $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
@@ -30,30 +25,4 @@ $(function () {
         $("#fuego4").attr("src", "images/favicon11.gif");
         $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
     });
-    
-    
-    /*function antorchas() {
-        $("#izqOff").attr("src", "images/Torch-izqOff.gif");
-        $("#derOff").attr("src", "images/Torch-derOff.gif");
-    }
-
-    function fuegos() {
-
-        if($("#tituloBlaze").getElementById === 'Bienvenido a Blaze!'){
-            $("#fuego1").attr("src", "images/favicon11.gif");
-            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
-            antorchas();
-        } else if($("#tituloBlaze").getElementById === 'Capítulos'){
-            $("#fuego2").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
-        } else if($("#tituloBlaze").getElementById === 'Galería'){
-            $("#fuego3").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
-            antorchas();
-        } else if($("#tituloBlaze").getElementById === 'Wyneg S. Rhuntar'){
-            $("#fuego4").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
-            antorchas();
-        }
-    }*/
 });
