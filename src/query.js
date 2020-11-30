502ddcac2af9124b06d4e32450c8e8d1107363be
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 16:04:34 -0300

    ...

--- src/query.js
+++ src/query.js
@@ -19,12 +19,18 @@ const connection = {
     host: 'ec2-52-5-176-53.compute-1.amazonaws.com',
     database: 'd7e454ve0aihrh',
     password: '10cbe08fc0ed8cb937ffa0e7fc33bf2a7718bf34d1f09e873249509cc836a084',
-    port: 5432
+    port: 5432,
 }
 
 const client = new Client(connection);
 
 client.connect();
+
+if(client.connect()){
+    console.log("conectado!");
+} else {
+    console.log("no conectado...");
+}
 
 client.query('SELECT numcaps FROM blazecaps WHERE numcaps = 1')
 .then(response => {
