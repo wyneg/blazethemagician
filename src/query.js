0f3898ca440a040fecd1c7e866d6253781953dec
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 15:43:41 -0300

    damn

--- src/query.js
+++ src/query.js
@@ -6,12 +6,20 @@ const router = require('./routes');
 
 //console.log('user : ' + process.env.DB_USER + 'pass : ' + process.env.DB_PASSWORD);
 
-const connection = {
+/*const connection = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
+}*/
+
+const connection = {
+    user: 'mlvqcsgesnyenk',
+    host: 'ec2-52-5-176-53.compute-1.amazonaws.com',
+    database: 'd7e454ve0aihrh',
+    password: '10cbe08fc0ed8cb937ffa0e7fc33bf2a7718bf34d1f09e873249509cc836a084',
+    port: 5432
 }
 
 const client = new Client(connection);
