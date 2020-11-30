6f799e83bf7f0baa7749068b85bf2636754a05fd
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 11:59:56 -0300

    damn...

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -2,22 +2,25 @@ $(function () {
     "use strict";
 
     $("#inicio1").on("click", function () {
+        console.log(document.URL.toString());
         $("#fuego1").attr("src", "images/favicon11.gif");
         $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
     });
     
     $("#capitulos1").on("click", function () {
+        console.log(document.URL.toString());
         $("#fuego2").attr("src", "images/favicon11.gif");
         $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
     });
     
     $("#galeria1").on("click", function () {
-        console.log(document.URL.toString);
+        console.log(document.URL.toString());
         $("#fuego3").attr("src", "images/favicon11.gif");
         $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
     });
     
     $("#autor1").on("click", function () {
+        console.log(document.URL.toString());
         $("#fuego4").attr("src", "images/favicon11.gif");
         $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
     });
