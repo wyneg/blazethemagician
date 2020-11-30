d342de5b379b0c99c361e3b9ffea2a43c9e08bac
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 09:28:05 -0300

    torchs guilt?

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -5,45 +5,39 @@ $(function () {
         e.preventDefault();
      });*/
 
-    function llamas() {
-        $('#inicio1').on('load', function() {
-            $("#fuego1").attr("src", "images/favicon11.gif");
-            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
-            antorchas();
-        });
     
-         $("#inicio1").on("click", function () {
-            $("#fuego1").attr("src", "images/favicon11.gif");
-            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
-            antorchas();
-        });
-        
-        $("#capitulos1").on("click", function () {
-            $("#fuego2").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
-        });
-        
-        $("#galeria1").on("click", function () {
-            $("#fuego3").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
-            antorchas();
-        });
-        
-        $("#autor1").on("click", function () {
-            $("#fuego4").attr("src", "images/favicon11.gif");
-            $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
-            antorchas();
-        });
-    }
+    $('#inicio1').on('load', function() {
+        $("#fuego1").attr("src", "images/favicon11.gif");
+        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+    });
 
+        $("#inicio1").on("click", function () {
+        $("#fuego1").attr("src", "images/favicon11.gif");
+        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+    });
+    
+    $("#capitulos1").on("click", function () {
+        $("#fuego2").attr("src", "images/favicon11.gif");
+        $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+    });
+    
+    $("#galeria1").on("click", function () {
+        $("#fuego3").attr("src", "images/favicon11.gif");
+        $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+    });
+    
+    $("#autor1").on("click", function () {
+        $("#fuego4").attr("src", "images/favicon11.gif");
+        $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
+    });
     
     
-    function antorchas() {
+    /*function antorchas() {
         $("#izqOff").attr("src", "images/Torch-izqOff.gif");
         $("#derOff").attr("src", "images/Torch-derOff.gif");
     }
 
-    /*function fuegos() {
+    function fuegos() {
 
         if($("#tituloBlaze").getElementById === 'Bienvenido a Blaze!'){
             $("#fuego1").attr("src", "images/favicon11.gif");
