bd5280ce0fd02b0c2661fa98f223bd54d9290665
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 09:19:17 -0300

    Damn llamas

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -5,34 +5,38 @@ $(function () {
         e.preventDefault();
      });*/
 
-    $('#inicio1').on('load', function() {
-        $("#fuego1").attr("src", "images/favicon11.gif");
-        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
-        antorchas();
-    });
-
-     $("#inicio1").on("click", function () {
-        $("#fuego1").attr("src", "images/favicon11.gif");
-        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
-        antorchas();
-	});
-	
-	$("#capitulos1").on("click", function () {
-        $("#fuego2").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
-	});
+    function llamas() {
+        $('#inicio1').on('load', function() {
+            $("#fuego1").attr("src", "images/favicon11.gif");
+            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+            antorchas();
+        });
     
-    $("#galeria1").on("click", function () {
-        $("#fuego3").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
-        antorchas();
-	});
-	
-	$("#autor1").on("click", function () {
-        $("#fuego4").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
-        antorchas();
-	});
+         $("#inicio1").on("click", function () {
+            $("#fuego1").attr("src", "images/favicon11.gif");
+            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+            antorchas();
+        });
+        
+        $("#capitulos1").on("click", function () {
+            $("#fuego2").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        });
+        
+        $("#galeria1").on("click", function () {
+            $("#fuego3").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+            antorchas();
+        });
+        
+        $("#autor1").on("click", function () {
+            $("#fuego4").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
+            antorchas();
+        });
+    }
+
+    
     
     function antorchas() {
         $("#izqOff").attr("src", "images/Torch-izqOff.gif");
