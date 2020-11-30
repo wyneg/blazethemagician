d2ac5288988aa12ae686b422f4b111a3bdcdbcac
wyneg <wynegsrhuntar@gmail.com>
2020-11-30 12:07:37 -0300

    switch

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,27 +1,42 @@
 $(function () {
     "use strict";
 
-    $("#inicio1").on("click", function () {
+    switch(URL){
+        case 'https://blazethemagician.herokuapp.com/capitulos':
+            $("#fuego2").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+            break;
+        case 'https://blazethemagician.herokuapp.com/galeria':
+            $("#fuego3").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+            break;
+        case 'https://blazethemagician.herokuapp.com/autor':
+            $("#fuego4").attr("src", "images/favicon11.gif");
+            $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
+            break;
+        default:
+            $("#fuego1").attr("src", "images/favicon11.gif");
+            $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+            break;
+    }
+
+    /*$("#inicio1").on("click", function () {
         console.log(document.URL.toString());
-        $("#fuego1").attr("src", "images/favicon11.gif");
-        $("#fuego2,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        
     });
     
     $("#capitulos1").on("click", function () {
         console.log(document.URL.toString());
-        $("#fuego2").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego3,#fuego4").attr("src", "images/espacio.png");
+        
     });
     
     $("#galeria1").on("click", function () {
         console.log(document.URL.toString());
-        $("#fuego3").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego4").attr("src", "images/espacio.png");
+        
     });
     
     $("#autor1").on("click", function () {
         console.log(document.URL.toString());
-        $("#fuego4").attr("src", "images/favicon11.gif");
-        $("#fuego1,#fuego2,#fuego3").attr("src", "images/espacio.png");
-    });
+        
+    });*/
 });
