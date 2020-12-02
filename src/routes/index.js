57787ccd2b8f99894e1ff1ad89b9e261bd959ec0
wyneg <wynegsrhuntar@gmail.com>
2020-12-02 17:05:14 -0300

    removing bodyparser, adding just express methods

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const bodyParser = require('body-parser');
+router.use(express.urlencoded());
 
-router.use(bodyParser.urlencoded({extended: true}));
-
-router.use(bodyParser.json());
+router.use(express.json());
 
 console.log(__dirname);
 
