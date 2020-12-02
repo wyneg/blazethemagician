cec4b5db4b288715189e9b9363cc4aefb859a64f
wyneg <wynegsrhuntar@gmail.com>
2020-12-02 17:10:23 -0300

    commiting input

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,9 +43,9 @@ router.get('/capitulos', (req, res) => {
 
 router.post('/capitulos', (req, res) => {
 
-    console.log("BODY : " + req.body);
+    console.log("BODY : " + req.body.buttonCap);
 
-    const cap = req.body.buttonCap;
+    /*const cap = req.body.buttonCap;
 
     const { Client } = require('pg');
 
@@ -65,7 +65,7 @@ router.post('/capitulos', (req, res) => {
 
     client.query(queryString, [cap])
     .then(response => console.log(response.rows))
-    .then(e => console.log(e));
+    .then(e => console.log(e));*/
    
 });
 
