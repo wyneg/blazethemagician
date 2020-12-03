d20cdf7fe1c7d468e35c59d0179a3c6a271455b3
wyneg <wynegsrhuntar@gmail.com>
2020-12-03 15:41:21 -0300

    extended

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use(express.urlencoded());
+router.use(express.urlencoded({extended: true}));
 
 router.use(express.json());
 
