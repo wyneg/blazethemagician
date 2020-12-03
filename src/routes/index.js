0fb2322a31fb50f3b59ff30188964347ae333909
wyneg <wynegsrhuntar@gmail.com>
2020-12-03 13:02:39 -0300

    async f(x)

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,7 +42,7 @@ router.get('/capitulos', (req, res) => {
     }*/
 });
 
-router.post('/capitulos', function (req, res) {
+router.post('/capitulos', async function (req, res) {
     //res.render('/app/src/views/capitulos.html', { title: 'Capítulos'});
 
     console.log("BODY : " + req.body.buttonCap);
