6096493ba488b8c80de735ad8a009eb74533fb32
wyneg <wynegsrhuntar@gmail.com>
2020-12-03 12:12:00 -0300

    damn

--- src/routes/index.js
+++ src/routes/index.js
@@ -68,7 +68,7 @@ router.post('/capitulos', function (req, res) {
     var capitulo = "";
 
     client.query(queryString, [cap])
-    .then(response => {capitulo = response.rows;})
+    .then(response => capitulo = response.rows)
     .catch(e => console.log(e)).then(() => client.end());
 
     //req.body.capis = response.rows;
