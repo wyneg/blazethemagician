cb59add40675c2cf9e912a900e3282c1851c8ea6
wyneg <wynegsrhuntar@gmail.com>
2020-12-07 11:26:22 -0300

    now it's for real... i think

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ app.use(require('./routes/index'));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Server en puerto : ', process.env.PORT);
+app.listen(/*process.env.PORT ||*/ 5000, () => {
+    console.log('Server en puerto : ', /*process.env.PORT*/ 5000);
 });
 
 /*const { Client } = require('pg');
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,8 @@ router.use(express.text());
 
 console.log(__dirname);
 
+
+//Remote
 router.get('/', (req, res) => {
     res.render('/app/src/views/index.html', { title: 'Bienvenido a Blaze!'});
 });
@@ -40,45 +42,12 @@ router.get('/capitulos/:id', (req, res) => {
         const queryString = 'SELECT numcaps FROM blazecaps WHERE numcaps = $1';
 
         client.query(queryString, [cap])
-        .then(response => response.rows)
+        .then(response => res.send(response.rows))
         .then(e => console.log(e));
     } catch(e){
         console.log(e);
     }
 });
-
-/*router.post('/capitulos', (req, res) => {
-
-    console.log("BODY : " + req.body);
-
-    const cap = req.body;
-
-    const { Client } = require('pg');
-
-    const connection = {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-    }
-
-    const client = new Client(connection);
-
-    client.connect();
-
-    const queryString = 'SELECT txtcaps FROM blazecaps WHERE numcaps = $1';
-
-    client.query(queryString, [cap])
-    .then(response => {
-        res.send(response.rows);
-    })
-    .catch(e => console.log(e));
-
-    
-
-    //res.end();
-});*/
 
 router.get('/galeria', (req, res) => {
     res.render('/app/src/views/galeria.html', { title: 'Galería'});
@@ -87,5 +56,56 @@ router.get('/galeria', (req, res) => {
 router.get('/autor', (req, res) => {
     res.render('/app/src/views/autor.html', { title: 'Wyneg S. Rhuntar'});
 });
+
+//Local
+
+/*router.get('/', (req, res) => {
+    res.render('/home/wyneg/blazethemagician/src/views/index.html', { title: 'Bienvenido a Blaze!'});
+});
+
+router.get('/capitulos', (req, res) => {
+    res.render('/home/wyneg/blazethemagician/src/views/capitulos.html', { title: 'Capítulos'});
+});
+
+router.get('/capitulos/:id', (req, res) => {
+
+    var cap = req.params.id;
+
+    try{
+
+        const { Client } = require('pg');
+
+        const connection = {
+            user: 'postgres',
+            host: 'localhost',
+            database: 'blazecaps',
+            password: '1983yattaze',
+            port: 5432,
+        }
+
+        const client = new Client(connection);
+
+        client.connect();
+
+        var queryString = 'SELECT txtcaps FROM caps WHERE numcaps = $1';
+
+        client.query(queryString, [cap])
+        .then(response => res.send(response.rows))
+        .then(e => console.log(e));
+
+    } catch(e){
+        console.log(e);
+    }
+    
+});
+
+router.get('/galeria', (req, res) => {
+    res.render('/home/wyneg/blazethemagician/src/views/galeria.html', { title: 'Galería'});
+});
+
+router.get('/autor', (req, res) => {
+    res.render('/home/wyneg/blazethemagician/src/views/autor.html', { title: 'Wyneg S. Rhuntar'});
+});*/
+
 
 module.exports = router;
