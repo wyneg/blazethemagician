505b5da33e0ef7c886ebbb330aedb9385250c8cf
wyneg <wynegsrhuntar@gmail.com>
2020-12-09 16:46:07 -0300

    more changes!!!

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,9 +10,39 @@ router.use(express.text());
 console.log(__dirname);
 
 
-/* //Remote
+//Remote
 router.get('/', (req, res) => {
     res.render('/app/src/views/index.html', { title: 'Bienvenido a Blaze!'});
+});
+
+router.get('/:id', (req, res) => {
+
+    var uno = req.params.id;
+    try{
+
+        const { Client } = require('pg');
+
+        const connection = {
+            user: 'postgres',
+            host: 'localhost',
+            database: 'blazecaps',
+            password: '1983yattaze',
+            port: 5432,
+        }
+
+        const client = new Client(connection);
+
+        client.connect();
+
+        var queryString = 'SELECT numcaps FROM caps ORDER BY numcaps DESC LIMIT $1';
+
+        client.query(queryString, [uno])
+        .then(response => res.send(response.rows))
+        .then(e => console.log(e));
+
+    } catch(e){
+        console.log(e);
+    }
 });
 
 router.get('/capitulos', (req, res) => {
@@ -55,19 +85,46 @@ router.get('/galeria', (req, res) => {
 
 router.get('/autor', (req, res) => {
     res.render('/app/src/views/autor.html', { title: 'Wyneg S. Rhuntar'});
-}); */
+});
 
-//Local
+/* //Local
 
 router.get('/', (req, res) => {
-
     res.render('/home/wyneg/blazethemagician/src/views/index.html', { title: 'Bienvenido a Blaze!'});
 });
 
-router.get('/capitulos', (req, res) => {
+router.get('/:id', (req, res) => {
 
+    var uno = req.params.id;
+    try{
+
+        const { Client } = require('pg');
+
+        const connection = {
+            user: 'postgres',
+            host: 'localhost',
+            database: 'blazecaps',
+            password: '1983yattaze',
+            port: 5432,
+        }
+
+        const client = new Client(connection);
+
+        client.connect();
+
+        var queryString = 'SELECT numcaps FROM caps ORDER BY numcaps DESC LIMIT $1';
+
+        client.query(queryString, [uno])
+        .then(response => res.send(response.rows))
+        .then(e => console.log(e));
+
+    } catch(e){
+        console.log(e);
+    }
+});
+
+router.get('/capitulos', (req, res) => {
     res.render('/home/wyneg/blazethemagician/src/views/capitulos.html', { title: 'Capítulos'});
-    
 });
 
 router.get('/capitulos/:id', (req, res) => {
@@ -108,7 +165,7 @@ router.get('/galeria', (req, res) => {
 
 router.get('/autor', (req, res) => {
     res.render('/home/wyneg/blazethemagician/src/views/autor.html', { title: 'Wyneg S. Rhuntar'});
-});
+}); */
 
 
 module.exports = router;
